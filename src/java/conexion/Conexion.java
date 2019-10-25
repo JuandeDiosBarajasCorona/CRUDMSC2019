93ab7b439b86b04c63ba29356838b8668e84f89a
JuandeBarco <lextadz@gmail.com>
2019-10-25 15:42:37 -0500

    Third

--- src/java/conexion/Conexion.java
+++ src/java/conexion/Conexion.java
@@ -6,11 +6,17 @@ public class Conexion {
     
     public DriverManagerDataSource conectar() {
         DriverManagerDataSource nav = new DriverManagerDataSource();
-        
+        /*
         nav.setDriverClassName("com.mysql.jdbc.Driver");
         nav.setUrl("jdbc:mysql://localhost:3306/msc2019");
         nav.setUsername("root");
         nav.setPassword("");
+        */
+        
+        nav.setDriverClassName("com.mysql.jdbc.Driver");
+        nav.setUrl("jdbc:mysql://juandebarco.com:3306/juandeba_crudmsc2019");
+        nav.setUsername("juandeba_admin");
+        nav.setPassword("time in london");
         
         return nav;
     }
